bf280652930d9a78f1bdf12722691c6594aa24dc
Dexx <dexx.electronic@gmail.com>
2025-09-01 10:56:32 +0200

    sync update

--- nvim/.config/nvim/lua/Dexx/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/Dexx/plugins/colorscheme.lua
@@ -1,18 +1,17 @@
 return {
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    --priority = 1000, -- ensures it loads before other UI plugins
-    config = function()
-      require("rose-pine").setup({
-        variant = "auto",         -- auto, main, moon, or dawn
-        dark_variant = "main",    -- choose your dark theme variant
-        disable_background = false,
-        disable_float_background = false,
-        disable_italics = false,
-      })
-      vim.cmd("colorscheme rose-pine")
-    end,
-  },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		--priority = 1000, -- ensures it loads before other UI plugins
+		config = function()
+			require("rose-pine").setup({
+				variant = "auto", -- auto, main, moon, or dawn
+				dark_variant = "main", -- choose your dark theme variant
+				disable_background = true,
+				disable_float_background = false,
+				disable_italics = false,
+			})
+			vim.cmd("colorscheme rose-pine")
+		end,
+	},
 }
-
